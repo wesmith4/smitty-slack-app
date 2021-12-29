2b03e14b83a622b8c04e0ce8ec42b399f9c7c7c2
Will Smith <william.e.smithiv@gmail.com>
2021-12-29 12:59:54 -0600

    modify app init

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ see: https://slack.dev/bolt-js/tutorial/getting-started
 // Initializes your app with your bot token and app token
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
   // socketMode: true,
   // appToken: process.env.SLACK_APP_TOKEN
+  port: process.env.PORT || 3000
 });
 
 const verifyUrl = async ({ event, ack, respond }) => {
