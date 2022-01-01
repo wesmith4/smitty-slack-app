30d2af5336b46a4f982629a873727205b53ce3c7
Will Smith <william.e.smithiv@gmail.com>
2022-01-01 08:37:29 -0600

    trying again

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,12 @@ module.exports = {
     },
     production: {
         client: 'pg',
-        connection: `${process.env.DATABASE_URL}?ssl=true`,
+        connection: {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        },
         migrations: {
             tableName: 'knex_migrations',
         },
