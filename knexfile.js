1eef7af4c4c96908d164b250186e05c530ea4cef
Will Smith <william.e.smithiv@gmail.com>
2022-01-01 08:26:47 -0600

    truing again

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,16 @@
 module.exports = {
     development: {
         client: 'pg',
-        connection: process.env.DATABASE_URL,
+        connection: 'postgres://localhost:5432/smitty_slack_app_dev',
         migrations: {
             tableName: 'knex_migrations',
         },
-        pool: {
-            min: 2,
-            max: 10,
+    },
+    test: {
+        client: 'pg',
+        connection: 'postgres://localhost:5432/smitty_slack_app_test',
+        migrations: {
+            tableName: 'knex_migrations',
         },
     },
     production: {
